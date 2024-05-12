eb3fcc3796620a020d3e3267164fe9387f981eee
pranavdua02 <duapranav11@gmail.com>
2024-05-12 16:21:06 +0530

    change base url

--- src/components/emails/OrderReceivedEmail.tsx
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -26,7 +26,7 @@ const OrderReceivedEmail = ({
   const baseUrl =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : 'https://casecobra.vercel.app'
+      : 'https://ecommerce-iphone.vercel.app'
 
   return (
     <Html>
