bf1f266e21cd972cb540ddfaf39efba9c27fc0b4
pranavdua02 <duapranav11@gmail.com>
2024-05-12 16:42:09 +0530

    updates

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       })
 
       await resend.emails.send({
-        from: 'CaseCobra <hello@joshtriedcoding.com>',
+        from: 'Ecommerce<duapranav11@gmail.com>',
         to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderReceivedEmail({
